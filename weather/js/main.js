c3bc16a59e7b3967b712239fde92bc1ae99bebef
Robert Regan <robert_regan86@yahoo.com>
2021-02-05 14:27:25 -0500

    updated main.js

--- weather/js/main.js
+++ weather/js/main.js
@@ -8,7 +8,7 @@ let spot = document.querySelector('input').value
 // key for  weatherapi.com ba151991832f4d089fe202428212901
 
 const url = `http://api.weatherapi.com/v1/current.json?key=ba151991832f4d089fe202428212901&q=${spot}`
-const url2 = `https://maps.googleapis.com/maps/api/place/findplacefromtext/output?${spot}`
+
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
